d049bf4734bddbb3d5b1a05260137c9bf54f0e5b
dyyyylan <jiangdylan00@gmail.com>
2024-11-05 21:06:54 -0500

    a

---
+++ vite-project/js/gundam.js
@@ -0,0 +1,152 @@
+export const gunpla = [
+    {
+        "name": "RX-78-2 Gundam",
+        "type": "Master Grade (MG)",
+        "price": "$40 - $50",
+        "series": "Mobile Suit Gundam"
+    },
+    {
+        "name": "Unicorn Gundam 02 Banshee Norn",
+        "type": "Perfect Grade (PG)",
+        "price": "$200 - $300",
+        "series": "Mobile Suit Gundam Unicorn"
+    },
+    {
+        "name": "Strike Freedom Gundam",
+        "type": "Real Grade (RG)",
+        "price": "$35 - $45",
+        "series": "Mobile Suit Gundam SEED Destiny"
+    },
+    {
+        "name": "Wing Gundam Zero EW",
+        "type": "Master Grade (MG)",
+        "price": "$60 - $70",
+        "series": "Mobile Suit Gundam Wing"
+    },
+    {
+        "name": "Gundam Exia",
+        "type": "Perfect Grade (PG)",
+        "price": "$250 - $300",
+        "series": "Mobile Suit Gundam 00"
+    },
+    {
+        "name": "Barbatos Lupus Rex",
+        "type": "High Grade (HG)",
+        "price": "$20 - $25",
+        "series": "Mobile Suit Gundam: Iron-Blooded Orphans"
+    },
+    {
+        "name": "Sazabi Ver. Ka",
+        "type": "Master Grade (MG)",
+        "price": "$90 - $120",
+        "series": "Mobile Suit Gundam: Char's Counterattack"
+    },
+    {
+        "name": "Nu Gundam",
+        "type": "Real Grade (RG)",
+        "price": "$60 - $70",
+        "series": "Mobile Suit Gundam: Char's Counterattack"
+    },
+    {
+        "name": "RX-78-2 Gundam Entry Grade",
+        "type": "Entry Grade (EG)",
+        "price": "$10 - $15",
+        "series": "Mobile Suit Gundam"
+    },
+    {
+        "name": "Astray Red Frame",
+        "type": "Master Grade (MG)",
+        "price": "$50 - $60",
+        "series": "Mobile Suit Gundam SEED Astray"
+    },
+    {
+        "name": "Zaku II",
+        "type": "High Grade (HG)",
+        "price": "$15 - $20",
+        "series": "Mobile Suit Gundam"
+    },
+    {
+        "name": "Gundam Mk-II Titans",
+        "type": "Real Grade (RG)",
+        "price": "$35 - $45",
+        "series": "Mobile Suit Zeta Gundam"
+    },
+    {
+        "name": "Tallgeese EW",
+        "type": "Master Grade (MG)",
+        "price": "$50 - $60",
+        "series": "Mobile Suit Gundam Wing"
+    },
+    {
+        "name": "RX-0 Unicorn Gundam",
+        "type": "Perfect Grade (PG)",
+        "price": "$220 - $280",
+        "series": "Mobile Suit Gundam Unicorn"
+    },
+    {
+        "name": "Sinanju Stein",
+        "type": "Real Grade (RG)",
+        "price": "$55 - $65",
+        "series": "Mobile Suit Gundam Unicorn"
+    },
+    {
+        "name": "Gundam Barbatos",
+        "type": "High Grade (HG)",
+        "price": "$20 - $25",
+        "series": "Mobile Suit Gundam: Iron-Blooded Orphans"
+    },
+    {
+        "name": "Gundam F91",
+        "type": "Master Grade (MG)",
+        "price": "$40 - $50",
+        "series": "Mobile Suit Gundam F91"
+    },
+    {
+        "name": "Gundam Aerial",
+        "type": "Entry Grade (EG)",
+        "price": "$10 - $15",
+        "series": "Mobile Suit Gundam: The Witch from Mercury"
+    },
+    {
+        "name": "ZZ Gundam Ver. Ka",
+        "type": "Master Grade (MG)",
+        "price": "$80 - $90",
+        "series": "Mobile Suit Gundam ZZ"
+    },
+    {
+        "name": "Gundam Astray Blue Frame",
+        "type": "High Grade (HG)",
+        "price": "$25 - $30",
+        "series": "Mobile Suit Gundam SEED Astray"
+    },
+    {
+        "name": "Freedom Gundam",
+        "type": "Real Grade (RG)",
+        "price": "$35 - $45",
+        "series": "Mobile Suit Gundam SEED"
+    },
+    {
+        "name": "Zeta Gundam",
+        "type": "Perfect Grade (PG)",
+        "price": "$250 - $300",
+        "series": "Mobile Suit Zeta Gundam"
+    },
+    {
+        "name": "Gundam Dynames",
+        "type": "High Grade (HG)",
+        "price": "$20 - $25",
+        "series": "Mobile Suit Gundam 00"
+    },
+    {
+        "name": "Gundam Deathscythe Hell EW",
+        "type": "Master Grade (MG)",
+        "price": "$50 - $60",
+        "series": "Mobile Suit Gundam Wing"
+    },
+    {
+        "name": "Gundam Ground Type",
+        "type": "Entry Grade (EG)",
+        "price": "$10 - $15",
+        "series": "Mobile Suit Gundam: The 08th MS Team"
+    }
+]
